809159ba6fe60ebfbc917d01bbc646ea9db0050b
vytch <vytch1906@gmail.com>
2018-09-24 20:28:06 +1000

    Remove default example

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -2,64 +2,24 @@
  * HomePage
  *
  * This is the first thing users see of our App, at the '/' route
+ *
+ * NOTE: while this component should technically be a stateless functional
+ * component (SFC), hot reloading does not currently support SFCs. If hot
+ * reloading is not a necessity for you then you can refactor it and remove
+ * the linting exception.
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { createStructuredSelector } from 'reselect';
-
-import injectReducer from 'utils/injectReducer';
-import injectSaga from 'utils/injectSaga';
-import {
-  makeSelectRepos,
-  makeSelectLoading,
-  makeSelectError,
-} from 'containers/App/selectors';
-import H2 from 'components/H2';
-import DigiPad from 'components/DigiPad';
+import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
-export class HomePage extends React.PureComponent {
-  /**
-   * when initial state username is not null, submit the form to load repos
-   */
-
+export default class HomePage extends React.PureComponent {
   render() {
-
-
     return (
-      <article>
-        <Helmet>
-          <title>Home Page</title>
-          <meta
-            name="description"
-            content="Calculator"
-          />
-        </Helmet>
-        <div>
-          <div>
-            <H2>
-              Test
-            </H2>
-            <DigiPad />
-          </div>
-        </div>
-      </article>
+      <h1>
+        <FormattedMessage {...messages.header} />
+      </h1>
     );
   }
 }
-
-HomePage.propTypes = {
-};
-
-
-
-const mapStateToProps = createStructuredSelector({
-});
-
-
-export default HomePage;
